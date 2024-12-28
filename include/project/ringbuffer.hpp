4be55203736a80ca8d4b4425700f17194d6d347d
junhao <ulricmen@gmail.com>
2024-12-28 05:35:03 +0000

    remove the 'size_' data member in RingBuffer

--- include/project/ringbuffer.hpp
+++ include/project/ringbuffer.hpp
@@ -2,30 +2,56 @@
 #define __PROJECT_RINGBUFFER_HPP__
 
 #include <atomic>
-#include <vector>
+#include <condition_variable>
+#include <mutex>
 
 namespace project {
 
 template <typename T>
 class RingBuffer {
  private:
-  std::vector<T> buffer_;
-  std::atomic<int> head_, tail_;
-  const int capacity_;
+  T* buffer_;
+  int head_, tail_;
+  const size_t capacity_;
+  std::mutex mtx_;
+  std::condition_variable is_full_, is_empty_;
 
  public:
+  // Allocate one extra slot for the judgement of full buffer.
   explicit RingBuffer(size_t buffer_size = 1024)
-      : buffer_(buffer_size), capacity_(buffer_size) {}
-  size_t size() const { return buffer_.size(); }
-  void enqueue(T&& item);
+      : buffer_(new T[buffer_size + 1]),
+        capacity_(buffer_size),
+        head_(0),
+        tail_(0) {}
+  ~RingBuffer() { delete[] buffer_; }
+  inline size_t size() const {
+    int size_ = tail_ - head_;
+    return size_ >= 0 ? size_ : capacity_ + size_;
+  }
+  inline size_t capacity() const { return capacity_; }
+  inline bool empty() const { return head_ == tail_; }
+  inline bool full() const { return (tail_ + 1) % capacity_ == head_; }
+  void enqueue(T& item);
   void dequeue(T& item);
 };
 
 template <typename T>
-void RingBuffer<T>::enqueue(T&& item) {}
+void RingBuffer<T>::enqueue(T& item) {
+  std::unique_lock lock(mtx_);
+  is_full_.wait(lock, [this]() { return !this->full(); });
+  buffer_[tail_] = item;
+  tail_ = (tail_ + 1) % capacity_;
+  is_empty_.notify_one();
+}
 
 template <typename T>
-void RingBuffer<T>::dequeue(T& item) {}
+void RingBuffer<T>::dequeue(T& item) {
+  std::unique_lock lock(mtx_);
+  is_empty_.wait(lock, [this]() { return !this->empty(); });
+  item = buffer_[head_];
+  head_ = (head_ + 1) % capacity_;
+  is_full_.notify_one();
+}
 
 }  // namespace project
 
--- tests/ringbuffer_test.cpp
+++ tests/ringbuffer_test.cpp
@@ -2,11 +2,43 @@
 
 #include <gtest/gtest.h>
 
+#include <chrono>
 #include <iostream>
+#include <thread>
+#include <vector>
 
-TEST(SampleTest, RingBuffer) {
+void producer(project::RingBuffer<int>& ringbuffer, int n) {
+  for (int i = 0; i < n; ++i) {
+    ringbuffer.enqueue(i);
+  }
+}
+
+void consumer(project::RingBuffer<int>& ringbuffer, int n) {
+  int val;
+  for (int i = 0; i < n; ++i) {
+    ringbuffer.dequeue(val);
+  }
+}
+
+TEST(MPMC, RingBuffer) {
   project::RingBuffer<int> ringbuffer(2048);
-  ASSERT_EQ(ringbuffer.size(), 2048);
+  ASSERT_EQ(ringbuffer.capacity(), 2048);
+  int n = 100000, num_threads = 20;
+  ASSERT_EQ(ringbuffer.size(), 0);
+  ASSERT_EQ(ringbuffer.full(), false);
+  ASSERT_EQ(ringbuffer.empty(), true);
+
+  std::vector<std::thread> producers, consumers;
+  for (int i = 0; i < num_threads; ++i) {
+    producers.push_back(std::thread(producer, std::ref(ringbuffer), n));
+    consumers.push_back(std::thread(consumer, std::ref(ringbuffer), n));
+  }
+
+  for (int i = 0; i < num_threads; ++i) {
+    producers[i].join();
+    consumers[i].join();
+  }
+  ASSERT_EQ(ringbuffer.size(), 0);
 }
 
 int main(int argc, char* argv[]) {
